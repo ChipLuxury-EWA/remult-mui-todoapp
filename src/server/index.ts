64233692d52ab2354e42983ce02d14bbafd3f460
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-02-12 22:55:18 +0200

    basic remult task entity

---
+++ src/server/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { api } from "./api";
+
+const PORT = 3002
+
+const app = express();
+app.use(api)
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
