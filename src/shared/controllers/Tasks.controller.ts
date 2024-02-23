3e5c1a7e7cead2f1c1c5a0c4db1dca44e6569091
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-02-23 16:25:01 +0200

    remove console

--- src/shared/controllers/Tasks.controller.ts
+++ src/shared/controllers/Tasks.controller.ts
@@ -5,7 +5,6 @@ export class TaskController {
     @BackendMethod({ allowed: Allow.authenticated })
     //write as a regular function, not anonyms fat arrow 
     static async setAllTasksCompleted(completed: boolean) {
-        console.log("first");
         const taskRepo = remult.repo(Task);
 
         for (const task of await taskRepo.find()) {
