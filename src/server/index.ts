7b0a8bbe4982b0ea687548ef14faf161c9d498cf
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-02-23 17:12:36 +0200

    prepare for deploy

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(session({ secret: process.env.SESSION_SECRET || "my secret" }));
 app.use(auth);
 app.use(api);
+app.use(express.static(process.cwd() + "/dist"))
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
