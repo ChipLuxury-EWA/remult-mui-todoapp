56eef2f91074b8d05c40655ccaa6d049b73530b3
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-03-17 20:08:18 +0200

    fix

--- src/shared/controllers/Tasks.controller.ts
+++ src/shared/controllers/Tasks.controller.ts
@@ -2,8 +2,8 @@ import { BackendMethod, remult, Allow } from "remult";
 import { Task } from "../entities/Task";
 
 export class TaskController {
-    @BackendMethod({ allowed: Allow.authenticated })
     //write as a regular function, not anonyms fat arrow 
+    @BackendMethod({ allowed: Allow.authenticated })
     static async setAllTasksCompleted(completed: boolean) {
         const taskRepo = remult.repo(Task);
 
